015bb26c44f547e0cf81429486ee9d50c837a059
Harlan Kellaway <hkellaway@users.noreply.github.com>
2020-09-08 21:20:10 -0400

    Create framework project

---
+++ AppVersion/AppVersion/AppVersion.h
@@ -0,0 +1,19 @@
+//
+//  AppVersion.h
+//  AppVersion
+//
+//  Created by Harlan Kellaway on 9/8/20.
+//  Copyright © 2020 Harlan Kellaway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppVersion.
+FOUNDATION_EXPORT double AppVersionVersionNumber;
+
+//! Project version string for AppVersion.
+FOUNDATION_EXPORT const unsigned char AppVersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppVersion/PublicHeader.h>
+
+
