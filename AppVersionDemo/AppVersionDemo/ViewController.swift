e01c2e3b03dbaad1813171f89be0536e36b33f6d
Harlan Kellaway <hkellaway@users.noreply.github.com>
2020-09-08 21:03:09 -0400

    Create Demo project

---
+++ AppVersionDemo/AppVersionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppVersionDemo
+//
+//  Created by Harlan Kellaway on 9/8/20.
+//  Copyright © 2020 Harlan Kellaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
